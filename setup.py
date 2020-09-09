4ed1182c9141d38d5ec6eb8126cb1c8050b73959
Darin McBeath <d.mcbeath@elsevier.com>
2020-09-09 14:12:03 -0400

    Updated for Spark 3.0
    The versions had gotten out of sycn so I adjusted it back to 1.0.2.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE.md') as f:
 
 setup(
     name='AnnotationQueryPython-spark3',
-    version='1.0.3',
+    version='1.0.2',
     description='Python implementation for AnnotationQuery',
     long_description=readme,
     author='Darin McBeath',
